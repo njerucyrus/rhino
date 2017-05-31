bf5c8c23296ad1d61ac97e23186283af0786f9aa
njerucyrus <njerucyrus5@hotmail.com>
2017-06-01 00:03:32 +0300

    updated autoloader

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -7,14 +7,17 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'App\\AppInterface\\BaseInterface' => $baseDir . '/src/App/AppInterface/BaseInterface.php',
+    'App\\AppInterface\\PaymentInterface' => $baseDir . '/src/App/AppInterface/PaymentInterface.php',
     'App\\AppInterface\\ReferralTreeInterface' => $baseDir . '/src/App/AppInterface/ReferralTreeInterface.php',
     'App\\AppInterface\\SiteInterface' => $baseDir . '/src/App/AppInterface/SiteInterface.php',
     'App\\AppInterface\\UserInterface' => $baseDir . '/src/App/AppInterface/UserInterface.php',
+    'App\\Controller\\PaymentController' => $baseDir . '/src/App/Controller/PaymentController.php',
     'App\\Controller\\ReferralTreeController' => $baseDir . '/src/App/Controller/ReferralTreeController.php',
     'App\\Controller\\SiteController' => $baseDir . '/src/App/Controller/SiteController.php',
     'App\\Controller\\UserController' => $baseDir . '/src/App/Controller/UserController.php',
     'App\\DBManager\\ComplexQuery' => $baseDir . '/src/App/DBManager/ComplexQuery.php',
     'App\\DBManager\\DB' => $baseDir . '/src/App/DBManager/DB.php',
+    'App\\Entity\\Payment' => $baseDir . '/src/App/Entity/Payment.php',
     'App\\Entity\\ReferralTree' => $baseDir . '/src/App/Entity/ReferralTree.php',
     'App\\Entity\\Site' => $baseDir . '/src/App/Entity/Site.php',
     'App\\Entity\\User' => $baseDir . '/src/App/Entity/User.php',
