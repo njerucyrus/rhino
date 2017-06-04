c449545cf7233c6f24e1fa866c02d2f11d145802
Samuel Githae <samgithae01@gmail.com>
2017-06-04 16:42:50 +0300

    updated index page

--- views/login.php
+++ views/login.php
@@ -1,151 +1,72 @@
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
-include __DIR__.'/includes/login.inc.php';
+/**
+ * Created by PhpStorm.
+ * User: New LAptop
+ * Date: 06/05/2017
+ * Time: 22:22
+ */
+require_once  '../vendor/autoload.php';
+include 'includes/register_user.inc.php';
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Heal | Login</title>
-    <?php include 'head_views.php'; ?>
-<style>
-    .avator{
-        width: 600px;
+<html>
+<?php include 'head_views.php' ?>
+<body class="page-body skin-facebook">
+<div class="page-container">
 
-        margin: 0 auto 10px;
-        display: block;
-        -moz-border-radius: 2%;
-        -webkit-border-radius: 2%;
-        border-radius: 2%;
-    }
-    .diva {
-        display:inline-block;
-        color:#444;
-        border:1px solid #CCC;
-        background:#DDD;
-        box-shadow: 0 0 5px -1px rgba(0,0,0,0.2);
-        cursor:pointer;
-        vertical-align:middle;
-        width: 300px;
+    <div class="main-content">
+        <div class="row">
+            <div class="col-md-12">
 
-        padding: 5px;
-        text-align: center;
-    }
-</style>
-</head>
-<body class="page-body login-page">
-
-
-<!-- This is needed when you send requests via Ajax -->
-<script type="text/javascript">
-    var baseurl = '';
-</script>
-
-<div class="login-container">
-
-    <div class="login-header login-caret">
-        <div class=" col-md-6 col-md-offset-3">
-
-            <img src="../public/assets/images/hospitalhand.jpg" class="avator"alt=""/>
-
-
+<div id="login-overlay" class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+            <h4 class="modal-title" id="myModalLabel" style="text-align: center;">Welcome back to <img src="../public/assets/img/logo.png"></h4>
         </div>
+        <div class="modal-body">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="well">
+                        <form id="loginForm" method="POST" action="/login/" novalidate="novalidate">
+                            <div class="form-group">
+                                <label for="username" class="control-label">Username</label>
+                                <input type="text" class="form-control" id="username" name="username" value="" required="" title="Please enter you username" placeholder="example@gmail.com">
+                                <span class="help-block"></span>
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="control-label">Password</label>
+                                <input type="password" class="form-control" id="password" name="password" value="" required="" title="Please enter your password">
+                                <span class="help-block"></span>
+                            </div>
+                            <div id="loginErrorMsg" class="alert alert-error hide">Wrong username og password</div>
 
+                            <button type="submit" class="btn btn-success btn-block">Login</button>
 
-    </div>
-
-
-    <div class=" login-form " >
-
-        <div class="login-content">
-
-            <div>
-                <?php
-                if(empty($success_msg) && !empty($error_msg)){
-                    ?>
-                    <div class="alert alert-danger alert-dismissable">
-                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                        <?php echo $error_msg ?>
+                        </form>
                     </div>
-                    <?php
-                }
-                elseif(empty($error_msg) and !empty($success_msg)){
-                    ?>
-                    <div class="alert alert-success alert-dismissable">
-                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                        <?php echo $success_msg  ?>
-                    </div>
-
-                    <?php
-                }
-
-                ?>
-            </div>
-
-            <p class="description" style="color: white; ">Dear user, log in to access the admin area!</p>
-            <div class="form-login-error">
-                <h3>Invalid login</h3>
-
-            </div>
-
-
-                <form class="diva" role="form" id="form_login" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" METHOD="post">
-
-                <div class="form-group ">
-
-                    <div class="input-group">
-                        <div class="input-group-addon">
-                            <i class="entypo-user"></i>
-                        </div>
-
-                        <input type="text" class="form-control" name="username" id="username" placeholder="Username"
-                               autocomplete="off"/>
-                    </div>
-
                 </div>
+                <div class="col-sm-6">
+                    <p class="lead">Register now </p>
+                    <ul class="list-unstyled" style="line-height: 2">
+                        <li><span class="fa fa-check text-success"></span> Get access to e-learning resources</li>
+                        <li><span class="fa fa-check text-success"></span> Get guidance on career choices</li>
+                        <li><span class="fa fa-check text-success"></span> Acquire the right skills</li>
+                        <li><span class="fa fa-check text-success"></span> Get access to learning opportunities</li>
 
-                <div class="form-group">
-
-                    <div class="input-group">
-                        <div class="input-group-addon">
-                            <i class="entypo-key"></i>
-                        </div>
-
-                        <input type="password" class="form-control" name="password" id="password" placeholder="Password"
-                               autocomplete="off"/>
-                    </div>
-
+                    </ul>
+                    <p><a href="../index2.php#join_us" class="btn btn-info btn-block">Yes please, register now</a></p>
                 </div>
-
-                <div class="form-group">
-                    <input type="submit" name="submit" value="Login" class="btn btn-primary btn-lg btn-block login-button"/>
-
-
-                </div>
-
-
-            </form>
-
-
-            <div class="login-bottom-links">
-
-                <a href="#" class="link">Forgot your password?</a>
-
-                <br/>
-
-
-
             </div>
-
         </div>
-
     </div>
-
 </div>
 
-<?php include 'footer_views.php'?>
-<script src="../public/assets/js/jquery-1.11.3.min.js"></script>
-<script src="../public/assets/js/bootstrap.min.js"></script>
-
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
