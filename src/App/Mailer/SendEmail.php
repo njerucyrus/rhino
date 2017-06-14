090e8db9eac41b38bc6a5c35fff301cf49775558
njerucyrus <njerucyrus5@hotmail.com>
2017-06-14 16:50:51 +0300

    implemented change password

--- src/App/Mailer/SendEmail.php
+++ src/App/Mailer/SendEmail.php
@@ -35,38 +35,6 @@ class SendEmail
     /**
      * @return mixed
      */
-    public function getReceiver()
-    {
-        return $this->receiver;
-    }
-
-    /**
-     * @param mixed $receiver
-     */
-    public function setReceiver($receiver)
-    {
-        $this->receiver = $receiver;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getSender()
-    {
-        return $this->sender;
-    }
-
-    /**
-     * @param mixed $sender
-     */
-    public function setSender($sender)
-    {
-        $this->sender = $sender;
-    }
-
-    /**
-     * @return mixed
-     */
     public function getMessage()
     {
         return $this->message;
